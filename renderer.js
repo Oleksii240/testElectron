82c9678f4c846453232c4d043cb817a2309950fb
musiienko25 <oleksii.musiienko@litslink.com>
2020-12-10 02:23:33 +0200

    scrollButton

--- renderer.js
+++ renderer.js
@@ -8,13 +8,13 @@ let tabGroup = new TabGroup({
 });
 
 let tab1 = tabGroup.addTab({
-    title: "Our Code World",
+    title: "React project",
     src: "http://aware-volcano.surge.sh/",
     visible: true
 });
 
 let tab2 = tabGroup.addTab({
-    title: "Local File",
+    title: "Reddit",
     src: "https://reddit.com",
     visible: true,
     webviewAttributes: {
@@ -22,11 +22,36 @@ let tab2 = tabGroup.addTab({
     }
 });
 
+
+
 tab2.webview.addEventListener('dom-ready', () => {
     tab2.webview.executeJavaScript(`var body = document.querySelector(".ListingLayout-outerContainer div");
     var secondBody = document.querySelector(".ListingLayout-outerContainer");
+    var elementButtonScroll = document.querySelector(".wBtTDilkW_rtT2k5x3eie")
     body.style.cssText = "color: blue; border: 1px solid black !important; background-color: black !important;";
-    secondBody.style.cssText = "color: blue; border: 1px solid black !important; background-color: black !important;"; 
-  console.log("This loads no problem!");`);
+    secondBody.style.cssText = "color: blue; border: 1px solid black !important; background-color: black !important;";
+    
+    var div = document.createElement("button");
+    div.className = "scrollButton";
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.background = "red";
+    div.style.color = "white";
+    div.style.position = "fixed";
+    div.style.zIndex = "2000";
+    div.innerHTML = "Scroll";
+
+
+    document.querySelector(".wBtTDilkW_rtT2k5x3eie").appendChild(div);
+
+    var myFancyFunction = function() {
+      window.scrollTo( 0, 10000 );
+    };
+
+    document.querySelector(".scrollButton").addEventListener('click', myFancyFunction);
+
+    console.log(scrollButton);
+
+    console.log(elementButtonScroll);`);
 });
 
